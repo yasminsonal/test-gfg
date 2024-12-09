da588cbc4b075719781774d5aecf211c8a3a6697
yasminsonal <23MH1A0595@acoe.edu.in>
2024-12-09 18:22:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Insert Interval/insert-interval.py
@@ -0,0 +1,57 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    def insertInterval(self, intervals, newInterval):
+        # Code here
+        merged = []
+        i = 0
+        n = len(intervals)
+
+        # Add all intervals that end before the new interval starts
+        while i < n and intervals[i][1] < newInterval[0]:
+            merged.append(intervals[i])
+            i += 1
+
+        # Merge overlapping intervals
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+
+        # Add the merged new interval
+        merged.append(newInterval)
+
+        # Add the remaining intervals
+        while i < n:
+            merged.append(intervals[i])
+            i += 1
+
+        return merged
+       
+        
+
+#{ 
+ # Driver Code Starts.
+if __name__ == '__main__': 
+    t = int(input ())
+    for _ in range (t):
+        N = int(input())
+        intervals = [list(map(int, input().split())) for i in range(N)]
+        newEvent = list(map(int, input().split()))
+        ob = Solution()
+        res = ob.insertInterval(intervals, newEvent)
+        print('[', end = '')
+        for i in range(len(res)):
+            print('[', end = '')
+            print(str(res[i][0])+','+str(res[i][1]), end = '')
+            print(']', end = '')
+            if i < len(res)-1:
+                print(',', end='')
+        print(']')
+        print("~")
+# } Driver Code Ends
