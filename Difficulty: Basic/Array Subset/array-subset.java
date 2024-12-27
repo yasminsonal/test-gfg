d48088d8c9a4b13682732f6f6fa85bc1e96ced00
yasminsonal <23MH1A0595@acoe.edu.in>
2024-12-27 22:54:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Array Subset/array-subset.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine().trim());
+        while (t-- > 0) {
+            String line = read.readLine().trim();
+            String[] numsStr = line.split(" ");
+            int[] a = new int[numsStr.length];
+            for (int i = 0; i < numsStr.length; i++) {
+                a[i] = Integer.parseInt(numsStr[i]);
+            }
+            line = read.readLine().trim();
+            numsStr = line.split(" ");
+            int[] b = new int[numsStr.length];
+            for (int i = 0; i < numsStr.length; i++) {
+                b[i] = Integer.parseInt(numsStr[i]);
+            }
+            Solution ob = new Solution();
+            if (ob.isSubset(a, b))
+                System.out.println("true");
+            else
+                System.out.println("false");
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public boolean isSubset(int a[], int b[]) {
+        // Your code here
+         HashSet<Integer> set=new HashSet<>();
+         for(int i:a)
+         {
+             set.add(i);
+         }
+         for(int i:b)
+         {
+             if(set.contains(i))
+             {
+                 continue;
+             }
+             else
+             {
+                 return false;
+             }
+         }
+         return true;
+    }
+}
