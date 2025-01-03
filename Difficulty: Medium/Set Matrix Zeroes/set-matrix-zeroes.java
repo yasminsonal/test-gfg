e5abbb968a4250bcc4830a65988fc42b39de744e
yasminsonal <23MH1A0595@acoe.edu.in>
2025-01-03 21:15:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Set Matrix Zeroes/set-matrix-zeroes.java
+++ Difficulty: Medium/Set Matrix Zeroes/set-matrix-zeroes.java
@@ -34,37 +34,36 @@ public class Main {
 
 // } Driver Code Ends
 
-
 //Back-end complete function Template for Java
 class Solution {
     public void setMatrixZeroes(int[][] mat) {
-        List<Integer> is=new ArrayList<Integer>();
-        List<Integer>js=new ArrayList<Integer>();
-        int n=mat.length,m=mat[0].length;
-        for(int i=0;i<n;i++)
+        int rl=mat.length;
+        int cl=mat[0].length;
+        
+        int[] row=new int[rl];
+        int[] col=new int[cl];
+        
+        for(int i=0;i<rl;i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=0;j<cl;j++)
             {
                 if(mat[i][j]==0)
                 {
-                    if(!is.contains(i)) is.add(i);
-                    if(!js.contains(j)) js.add(j);
+                    row[i]=1;
+                    col[j]=1;
                 }
             }
         }
-        for(int i=0;i<is.size();i++)
+        for(int i=0;i<rl;i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=0;j<cl;j++)
             {
-                mat[is.get(i)][j]=0;
+                if(row[i]==1||col[j]==1)
+                {
+                    mat[i][j]=0;
+                }
             }
         }
-        for(int j=0;j<js.size();j++)
-        {
-            for(int i=0;i<n;i++)
-            {
-                mat[i][js.get(j)]=0;
-            }
-        }
+        
     }
 }
