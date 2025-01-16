66e8e8370419bdcec67353a5a0a7efbe4b2a5cc7
yasminsonal <23MH1A0595@acoe.edu.in>
2025-01-16 21:12:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine().trim());
+
+        while (t-- > 0) {
+            String line = read.readLine().trim();
+            String[] numsStr = line.split(" ");
+            int[] nums = new int[numsStr.length];
+            for (int i = 0; i < numsStr.length; i++) {
+                nums[i] = Integer.parseInt(numsStr[i]);
+            }
+
+            int d = Integer.parseInt(read.readLine().trim());
+
+            Solution ob = new Solution();
+            ArrayList<Integer> result = ob.subarraySum(nums, d);
+            // Print all elements in the result list
+            for (int i : result) {
+                System.out.print(i + " ");
+            }
+            System.out.println(); // Print a new line after the result
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    static ArrayList<Integer> subarraySum(int[] arr, int target) {
+        // code here
+        ArrayList<Integer> sa=new ArrayList<>();
+        int c=0;
+        for(int i=1;i<=arr.length;i++){
+            c=0;
+            for(int j=i-1;j<arr.length;j++)
+            {
+                c+=arr[j];
+                if(c==target)
+                {
+                    sa.add(i);
+                    sa.add(j+1);
+                    return sa;
+                }
+            }
+        }
+        sa.add(-1);
+        return sa;
+    }
+}
