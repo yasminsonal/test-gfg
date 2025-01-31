56fa31b71da29e38ec69bf9654db8b60f19c6aea
yasminsonal <23MH1A0595@acoe.edu.in>
2025-01-31 11:10:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Equilibrium Point/equilibrium-point.java
+++ Difficulty: Easy/Equilibrium Point/equilibrium-point.java
@@ -42,22 +42,35 @@ class Solution {
     // Function to find equilibrium point in the array.
     public static int findEquilibrium(int arr[]) {
         // code here
-        int totalsum=0;
-        int leftsum=0;
-        int n=arr.length;
-        for(int i=0;i<n;i++)
-        {
-            totalsum=totalsum+arr[i];
+        int totalSum = 0;
+        for (int num : arr) {
+            totalSum += num;
         }
-        for(int i=0;i<n;i++)
-        {
-            totalsum-=arr[i];
-            if(leftsum==totalsum)
-            {
+
+        int leftSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            totalSum -= arr[i];
+            if (leftSum == totalSum) {
                 return i;
             }
-            leftsum+=arr[i];
+            leftSum += arr[i];
         }
+
         return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array:");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int equilibriumIndex = findEquilibrium(arr);
+        System.out.println("The first equilibrium index is: " + equilibriumIndex);
     }
 }
