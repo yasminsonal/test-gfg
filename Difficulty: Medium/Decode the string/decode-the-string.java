48c751bc1d54fb4840dbd628c5b24101f6f97e41
yasminsonal <23MH1A0595@acoe.edu.in>
2025-03-01 02:18:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Decode the string/decode-the-string.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        int t = Integer.parseInt(in.readLine());
+        while (t-- > 0) {
+            String s = in.readLine();
+
+            Solution ob = new Solution();
+            out.println(ob.decodeString(s));
+
+            out.println("~");
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+
+class Solution {
+    static String decodeString(String s) {
+        // code here
+        Stack<Integer> countStack = new Stack<>();
+        Stack<StringBuilder> stringStack = new Stack<>();
+        StringBuilder currentString = new StringBuilder();
+        int k = 0;
+
+        for (char ch : s.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                k = k * 10 + ch - '0';
+            } else if (ch == '[') {
+                countStack.push(k);
+                stringStack.push(currentString);
+                currentString = new StringBuilder();
+                k = 0;
+            } else if (ch == ']') {
+                StringBuilder decodedString = stringStack.pop();
+                int repeatTimes = countStack.pop();
+                for (int i = 0; i < repeatTimes; i++) {
+                    decodedString.append(currentString);
+                }
+                currentString = decodedString;
+            } else {
+                currentString.append(ch);
+            }
+        }
+        return currentString.toString();
+    }
+}
