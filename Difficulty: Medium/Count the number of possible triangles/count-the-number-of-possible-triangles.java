2861b0a76696cba7e9cbe9c2c9bbf4f448693c28
yasminsonal <23MH1A0595@acoe.edu.in>
2025-04-14 14:58:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.java
+++ Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.java
@@ -27,27 +27,33 @@ class Solution {
     // Function to count the number of possible triangles.
     static int countTriangles(int arr[]) {
         // code here
+        int n = arr.length;
+
+        // If there are fewer than 3 sides, no triangle can be formed.
+        if (n < 3) return 0;
+
+        // Sort the array in ascending order
         Arrays.sort(arr);
-        int count=0;
-        for(int k=arr.length-1;k>=2;k--)
-        {
-            int st=0;
-            int ed=k-1;
-            while(ed>st)
-            {
-                int sum=arr[st]+arr[ed];
-                if(sum>arr[k])
-                {
-                    count+=ed-st;
-                    ed--;
-                }
-                else
-                {
-                    st++;
+
+        int count = 0;
+
+        // Fix the third side (largest side of the triangle)
+        for (int k = n - 1; k >= 2; k--) {
+            int i = 0; // Start pointer
+            int j = k - 1; // End pointer before the largest side
+
+            // Check pairs (i, j) such that arr[i] + arr[j] > arr[k]
+            while (i < j) {
+                if (arr[i] + arr[j] > arr[k]) {
+                    // All pairs between i and j can form a triangle with arr[k]
+                    count += (j - i);
+                    j--; // Decrease j to check smaller pairs
+                } else {
+                    i++; // Increase i to increase the sum
                 }
             }
         }
+
         return count;
-        
     }
 }
