531778612e1e67fcf20c4a84c4523b73acf23fec
yasminsonal <23MH1A0595@acoe.edu.in>
2025-06-14 12:22:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Peak element/peak-element.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    public int peakElement(int[] arr) {
+        // code here
+        int low=0;
+        int high = arr.length - 1;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(arr[mid] > arr[mid+1]){
+                high = mid;
+                
+            }
+            else{
+                low = mid+1;
+            }
+        }
+            return low;
+        
+    }
+}
