7dc12d2f225368a6e6cc4323f54cc174f70d31a2
Jannat Firdous <23MH1A0595@acoe.edu.in>
2025-12-08 21:36:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print With Space/print-with-space.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public void utility(Scanner scn) {
+        // code here
+        
+         String a=scn.next();
+         String b=scn.next();
+        
+        System.out.println(a+" "+b);
+    }
+}
