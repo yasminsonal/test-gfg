05e94cff04470dc5dc45bffe05b6de0d18cb98b0
Jannat Firdous <23MH1A0595@acoe.edu.in>
2025-12-08 22:08:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/The If Statement/the-if-statement.java
+++ Difficulty: Basic/The If Statement/the-if-statement.java
@@ -1,13 +1,18 @@
-// User function Template for Java
-class Solution {
-    public static void utility(int number) {
-        // Write your if statement below
+import java.util.*;
 
-        if(number>100){
+public class Solution {
+    public static void solve() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // check whether n is big or a number
+        if(n>100){
             System.out.println("Big");
         }
-        
+        if(n>100){
             System.out.println("Number");
-        
+        }
+        else{
+            System.out.println("Number");
+        }
     }
 }
