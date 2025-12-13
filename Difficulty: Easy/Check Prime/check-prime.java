3386de00cea89bf709b8ded25a5e3eae6722ae4f
Jannat Firdous <23MH1A0595@acoe.edu.in>
2025-12-13 23:54:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check Prime/check-prime.java
+++ Difficulty: Easy/Check Prime/check-prime.java
@@ -1,16 +1,17 @@
-class Solution {
-    public static boolean prime(int n) {
-        // Write your code here
-        if(n==1){
-            return false;
-        }
+import java.util.*;
+
+public class Solution {
+    public static void main(String args[]) {
+        // Your Code Here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
         for(int i=2;i<n;i++){
-            if(n%i==0){
-                return false;
-            }
-            
+        if(n%i==0){
+            System.out.println("False");
+            System.exit(0);
         }
-        return true;
         
+        }
+        System.out.println("True");
     }
 }
