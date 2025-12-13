b3c851355dc641b1629c3975c130512dc5ed6b89
Jannat Firdous <23MH1A0595@acoe.edu.in>
2025-12-13 23:44:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Last Digit of a number/last-digit-of-a-number.java
+++ Difficulty: Basic/Last Digit of a number/last-digit-of-a-number.java
@@ -1,19 +1,7 @@
 class Solution {
-    public static void utility(int n) {
-
-        // just complete below statement
-        int ans;
-        if(n > 0){
-            ans = n% 10;
-        }
-        else{
-            ans = (-1) * (n%10);
-        }
-
-    
-  
-
-            // below statement print the result
-            System.out.println(ans);
+    public int lastDigit(int n) {
+        // Code here
+        int m=Math.abs(n%10);
+        return m;
     }
 }
