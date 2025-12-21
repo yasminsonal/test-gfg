1fc2f2b1ecf34f004dd443553d7f01b7f37a1c74
Jannat Firdous <23MH1A0595@acoe.edu.in>
2025-12-21 09:21:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Input String/input-string.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        // Take String Input and Print it
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.println(s+" ");
+    }
+}
