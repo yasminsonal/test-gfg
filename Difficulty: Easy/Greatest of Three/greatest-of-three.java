0b2552abdef92f4661a3489360ec1ee5c28fd125
Jannat Firdous <23MH1A0595@acoe.edu.in>
2025-12-21 09:27:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Greatest of Three/greatest-of-three.java
@@ -0,0 +1,22 @@
+//Back-end complete function Template for Java
+import java.util.*;
+
+public class Solution {
+    public static void main(String args[]) {
+        // Your code here
+        Scanner sc=new Scanner(System.in);
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        int n3=sc.nextInt();
+        
+        if(n1>n2 && n1>n3){
+            System.out.println(n1);
+        }
+        else if(n2>n1 && n2>n3){
+            System.out.println(n2);
+        }
+        else{
+            System.out.println(n3);
+        }
+    }
+}
