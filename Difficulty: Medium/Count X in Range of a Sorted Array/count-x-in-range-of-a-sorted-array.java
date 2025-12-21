9f93f9fc8cf599612940d2796a64c703212fcdea
Jannat Firdous <23MH1A0595@acoe.edu.in>
2025-12-21 09:13:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count X in Range of a Sorted Array/count-x-in-range-of-a-sorted-array.java
@@ -0,0 +1,52 @@
+class Solution {
+    public ArrayList<Integer> countXInRange(int[] arr, int[][] queries) {
+        // code here
+         ArrayList<Integer> ans = new ArrayList<>();
+
+        for (int[] q : queries) {
+            int l = q[0], r = q[1], x = q[2];
+
+            int first = firstOccurrence(arr, l, r, x);
+            if (first == -1) {
+                ans.add(0);
+                continue;
+            }
+
+            int last = lastOccurrence(arr, l, r, x);
+            ans.add(last - first + 1);
+        }
+
+        return ans;
+    }
+
+    private int firstOccurrence(int[] arr, int l, int r, int x) {
+        int res = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] == x) {
+                res = mid;
+                r = mid - 1;
+            } else if (arr[mid] < x) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return res;
+    }
+    private int lastOccurrence(int[] arr, int l, int r, int x) {
+        int res = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] == x) {
+                res = mid;
+                l = mid + 1;
+            } else if (arr[mid] < x) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return res;
+    }
+}
