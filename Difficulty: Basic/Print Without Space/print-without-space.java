5de74b600ab2077baf71b60d406d32becfbbd533
Jannat Firdous <23MH1A0595@acoe.edu.in>
2025-12-22 23:48:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Without Space/print-without-space.java
@@ -0,0 +1,8 @@
+class Solution {
+    public void utility(Scanner scn) {
+        // code here
+        String a=scn.nextLine();
+        String b=scn.nextLine();
+        System.out.println(a+b);
+    }
+}
