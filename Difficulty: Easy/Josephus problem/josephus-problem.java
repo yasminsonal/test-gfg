98bc7be2be2e22cd72ff82c52249101bf4fd49c1
Jannat Firdous <23MH1A0595@acoe.edu.in>
2026-01-24 16:37:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Josephus problem/josephus-problem.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int josephus(int n, int k) {
+        // code here
+        int res=0;
+        for(int i=1;i<=n;i++){
+            res =(res+k)%i;
+        }
+        return res+1;
+    }
+}
