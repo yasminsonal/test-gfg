18d78e37b83e633e2c22e8cd55b5b874be05e64c
Jannat Firdous <23MH1A0595@acoe.edu.in>
2026-01-27 19:34:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Word Search/word-search.java
+++ Difficulty: Medium/Word Search/word-search.java
@@ -1,72 +1,49 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int tc = sc.nextInt(); // Number of test cases
-        while (tc-- > 0) {
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-            char[][] mat = new char[n][m];
-
-            // Reading the matrix
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    mat[i][j] = sc.next().charAt(0);
+class Solution {
+    public boolean isWordExist(char[][] mat, String word) {
+        // Code here
+        int n = mat.length;
+        int m = mat[0].length;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == word.charAt(0)) {
+                    if (dfs(mat, word, i, j, 0)) {
+                        return true;
+                    }
                 }
             }
-
-            String word = sc.next();
-            Solution obj = new Solution();
-            boolean ans = obj.isWordExist(mat, word);
-            if (ans)
-                System.out.println("true");
-            else
-                System.out.println("false");
-
-            System.out.println("~");
-        }
-        sc.close();
-    }
-}
-// } Driver Code Ends
-
-
-class Solution {
-    static public boolean isWordExist(char[][] mat, String word) {
-        // Code here
-         int n=mat.length;
-        int m=mat[0].length;
-        boolean[][] vis=new boolean[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(dfs(i,j,0,word,mat,vis)) return true;
-            }
         }
         return false;
     }
-    
-    static boolean dfs(int i,int j,int s,String word,char[][] mat,boolean[][] vis){
-        if(s==word.length()) return true;
-        if(word.charAt(s)!=mat[i][j]) return false;
-        vis[i][j]=true;
-        int[] dir={0,1,0,-1,0};
-        int n=mat.length;
-        int m=mat[0].length;
-        for(int d=0;d<4;d++){
-            int nI=dir[d]+i;
-            int nJ=dir[d+1]+j;
-            if(isValid(nI,nJ,n,m) && !vis[nI][nJ]){
-                if(dfs(nI,nJ,s+1,word,mat,vis)) return true;
+
+    private boolean dfs(char[][] mat, String word, int r, int c, int index) {
+        if (index == word.length() - 1) {
+            return true;
+        }
+        
+        char temp = mat[r][c];
+        mat[r][c] = '#';
+        
+        int[] dr = {-1, 1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
+        
+        for (int k = 0; k < 4; k++) {
+            int newR = r + dr[k];
+            int newC = c + dc[k];
+            
+            if (newR >= 0 && newR < mat.length && 
+                newC >= 0 && newC < mat[0].length && 
+                mat[newR][newC] == word.charAt(index + 1)) {
+                
+                if (dfs(mat, word, newR, newC, index + 1)) {
+                    return true;
+                }
             }
         }
-        vis[i][j]=false;
+        
+        // Backtrack: Unmark the current cell so other paths can use it
+        mat[r][c] = temp;
+        
         return false;
-    }
-    
-    static boolean isValid(int i,int j,int n,int m){
-        return i>=0 && j>=0 && i<n && j<m;
     }
 }
