2482ad96dcc53beb280c48722f61e84f75bece18
Jannat Firdous <23MH1A0595@acoe.edu.in>
2026-02-20 14:55:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Form the Largest Number/form-the-largest-number.java
+++ Difficulty: Medium/Form the Largest Number/form-the-largest-number.java
@@ -1,56 +1,20 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        sc.nextLine(); // Consume newline
-        while (t-- > 0) {
-            String input = sc.nextLine();
-            String[] numbers = input.split(" ");
-            int[] arr = new int[numbers.length];
-            for (int i = 0; i < numbers.length; i++) {
-                arr[i] = Integer.parseInt(numbers[i]);
-            }
-
-            Solution ob = new Solution();
-            String ans = ob.findLargest(arr);
-            System.out.println(ans);
-            System.out.println("~");
-        }
-        sc.close();
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    String findLargest(int[] arr) {
+    public String findLargest(int[] arr) {
         // code here
-        int n=arr.length;
-        if(n==0){
+        String s[] = new String[arr.length];
+        int x=0;
+        StringBuilder ans=new StringBuilder();
+        for(int n:arr){
+            s[x++] = String.valueOf(n);}
+            if(s[0].equals("0"))
             return "0";
-        }
-        String[] arr1=new String[arr.length];
-        for(int i=0;i<n;i++)
-        {
-            arr1[i]=String.valueOf(arr[i]);
-        }
-        Arrays.sort(arr1,(a,b)->(b+a).compareTo(a+b));
+        Arrays.sort(s, (a,b) -> (b+a).compareTo(a+b));
         
-        if(arr1[0].equals("0")){
-            return "0";
-        }
-        StringBuilder sb=new StringBuilder();
-        for(int i=0;i<n;i++)
+        for(String ss:s)
         {
-            sb.append(arr1[i]);
+            ans.append(ss);
         }
-        return sb.toString();
+        
+        return ans.toString();
     }
 }
